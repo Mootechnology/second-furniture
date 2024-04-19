917fbb028841416ac0b419f0977ac08f62f4c4f8
MUHAMMADNOMAN <ns666554@gmail.com>
2024-04-19 12:45:23 +0500

    staterkit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\FileManagerController;
+use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\UploadController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +20,66 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// for components testing purpose
+Route::view('/', 'welcome');
+
+Route::controller(AuthController::class)
+    ->prefix('auth')
+    ->name('auth.')
+    ->group(function () {
+        Route::get('login',  'loginView')->name('login');
+        Route::post('login-user', 'userLogin')->name('login.user');
+        Route::get('register',  'registerView')->name('register');
+        Route::post('check-register', 'checkRegister')->name('check.register');
+        Route::get('logout', 'logout')->name('logout');
+    });
+
+Route::middleware('auth')->group(function () {
+    Route::controller(UserController::class)
+        ->prefix('user')
+        ->name('user.')
+        ->group(function () {
+            Route::get('index', 'index')->name('index');
+            Route::get('create', 'create')->name('create');
+            Route::post('store', 'store')->name('store');
+            Route::get('edit/{user}', 'edit')->name('edit');
+            Route::post('update/{user}', 'update')->name('update');
+            Route::get('delete/{user}', 'destroy')->name('delete');
+        });
+
+    Route::get('filemanager', [FileManagerController::class, 'index'])->name('file.index');
+    Route::post('filemanager/upload', [FileManagerController::class, 'upload'])->name('file.upload');
+    Route::post('file/store', [FileManagerController::class, 'store'])->name('file.store');
+    Route::get('/file/get-image/{id}', [FileManagerController::class, 'getImage']);
+    Route::delete('filemanager/{file}', [FileManagerController::class, 'delete'])->name('filemanager.delete');
+
+
+    Route::controller(RoleController::class)
+        ->prefix('role')
+        ->name('role.')
+        ->group(function () {
+            Route::get('index', 'index')->name('index');
+            Route::get('create', 'create')->name('create');
+            Route::post('store', 'store')->name('store');
+            Route::get('edit/{role}', 'edit')->name('edit');
+            Route::post('update/{role}', 'update')->name('update');
+            Route::get('delete/{role}', 'destroy')->name('delete');
+        });
+
+    Route::controller(PermissionController::class)
+        ->prefix('permission')
+        ->name('permission.')
+        ->group(function () {
+            Route::get('index', 'index')->name('index');
+            Route::get('create', 'create')->name('create');
+            Route::post('store', 'store')->name('store');
+            Route::get('edit/{permission}', 'edit')->name('edit');
+            Route::post('update{permission}', 'update')->name('update');
+            Route::get('delete/{permission}', 'destroy')->name('delete');
+            Route::get('synchronize', 'synchronize')->name('synchronize');
+        });
+
+});
 
 Route::view('/', 'frontend.index')->name('index');
 Route::view('/category', 'frontend.category')->name('category');
